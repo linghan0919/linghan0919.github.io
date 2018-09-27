0d83849c877031e25b1f6f863e8d7a7ed3dcbe03
linghan0919 <linghan0919@163.com>
2018-09-26 20:14:53 -0400

    update research/course/internship

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,6 +1,21 @@
 jQuery(document).ready(function($) {
 
-
+	/*====== Show More/Less Projects ======= */
+	$('a.show-more').on("click", function() {
+		if ($(this).siblings('div.show-more').hasClass('hidden')) {
+			$(this).siblings('div.show-more').removeClass('hidden');
+			$(this).children('i').addClass('fa-angle-double-up');
+			$(this).children('i').removeClass('fa-angle-double-down');
+			$(this).children('span').html(' Show Less');
+		}
+		else {
+			$(this).siblings('div.show-more').addClass('hidden');
+			$(this).children('i').addClass('fa-angle-double-down');
+			$(this).children('i').removeClass('fa-angle-double-up');
+			$(this).children('span').html(' Show More');
+		}
+	});
+	
     /*======= Skillset *=======*/
     
     $('.level-bar-inner').css('width', '0');
@@ -22,38 +37,4 @@ jQuery(document).ready(function($) {
     /* Bootstrap Tooltip for Skillset */
     $('.level-label').tooltip();
     
-    /* jQuery RSS - https://github.com/sdepold/jquery-rss */
-    $("#rss-feeds").rss(
-    
-        //Change this to your own rss feeds
-        "http://feeds.feedburner.com/TechCrunch/startups",
-        
-        {
-        // how many entries do you want?
-        // default: 4
-        // valid values: any integer
-        limit: 3,
-        
-        // the effect, which is used to let the entries appear
-        // default: 'show'
-        // valid values: 'show', 'slide', 'slideFast', 'slideSynced', 'slideFastSynced'
-        effect: 'slideFastSynced',
-        
-        // outer template for the html transformation
-        // default: "<ul>{entries}</ul>"
-        // valid values: any string
-        layoutTemplate: "<div class='item'>{entries}</div>",
-        
-        // inner template for each entry
-        // default: '<li><a href="{url}">[{author}@{date}] {title}</a><br/>{shortBodyPlain}</li>'
-        // valid values: any string
-        entryTemplate: '<h3 class="title"><a href="{url}" target="_blank">{title}</a></h3><div><p>{shortBodyPlain}</p><a class="more-link" href="{url}" target="_blank"><i class="fa fa-external-link"></i>Read more</a></div>'
-        
-        }
-    );
-    
-    /* Github Activity Feed - https://github.com/caseyscarborough/github-activity */
-    GitHubActivity.feed({ username: "caseyscarborough", selector: "#ghfeed" });
-
-
 });
